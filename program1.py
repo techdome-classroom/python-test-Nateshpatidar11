cadd47fa974816d0ea251b998d32cafc21d80b46
Nateshpatidar11 <150167993+Nateshpatidar11@users.noreply.github.com>
2024-07-26 13:34:32 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,6 +1,21 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+    
+        
+        def test(a, c):
+            if a < 0 or a >= len(grid) or c < 0 or c >= len(grid[0]) or grid[a][c] == 'W':
+                return
+            grid[a][c] = 'W'  
+            test(a + 1, c)
+            test(a - 1, c)
+            test(a, c + 1)
+            test(a, c - 1)
+        
+        num_islands = 0
+        for a in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[a][c] == 'L':
+                    num_islands += 1
+                    test(a, c)
+        
+        return num_islands
